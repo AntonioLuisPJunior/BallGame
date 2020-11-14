059d8bfab68e1ddebb1ac09752163a4a43e6c8af
Antonio <antoniojunior11@aluno.uema.br>
2020-11-13 23:57:23 -0300

    posicionamento das bolas nas quadras

--- desenhos/status/GameStatus.java
+++ desenhos/status/GameStatus.java
@@ -14,9 +14,9 @@ public class GameStatus extends Status {
             Retangulo.quadra = new Retangulo(0, 0, game.getWidth(), game.getHeight());
         }
         Bola.bolas.add(new Bola(250, 380, 100, 2, -42, Color.BLUE));
-        Bola.bolas.add(new Bola(200, 80, 50, 2, -84, Color.RED));
-        Bola.bolas.add(new Bola(500, 170, 70, 2, -42, Color.YELLOW));
-        Bola.bolas.add(new Bola(500, 170, 90, 2, -42, Color.GREEN));
+        Bola.bolas.add(new Bola(100, 80, 50, 2, -84, Color.RED));
+        Bola.bolas.add(new Bola(300, 170, 70, 2, -42, Color.YELLOW));
+        Bola.bolas.add(new Bola(500, 170, 90, 2, 60, Color.GREEN));
     }
 
     @Override
